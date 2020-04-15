265a0113b70bc11fc3d19bbf7f498c492d766267
ArilAsghaf <61927378+ArilAsghaf@users.noreply.github.com>
2020-04-15 15:28:59 +0700

    Add files via upload

---
+++ Karyawan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2915
+{
+    class Karyawan
+    {
+        public Karyawan(string name, int no, int nik, int gaji)
+        {
+            Nama = name;
+            No = no;
+            NIK = nik;
+            Gaji = gaji;
+        }
+        public string Nama { get; set; }
+        public int No { get; set; }
+        public int NIK { get; set; }
+        public int Gaji { get; set; }
+        public void gaji()
+        {
+            Console.WriteLine("Karyawan has name : {0} no : {1} nik : {2} gaji {3}", Nama, No, NIK, Gaji);
+        }
+        public void gajinaik()
+        {
+        Console.WriteLine("Karyawan has name : {0} no : {1} nik : {2} gaji {3}", Nama, No, NIK, Gaji + (Gaji * 10 / 100));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2915
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Karyawan karyawan1 = new Karyawan(1, 2915, "Aril", 10000000);
+            karyawan1.gaji();
+
+            Karyawan karyawan2 = new Karyawan(2, 3301, "Nednod", 8000000);
+            karyawan2.gaji();
+
+            Karyawan karyawan3 = new Karyawan(1, 2915, "Aril", 10000000);
+            karyawan3.gajinaik();
+
+            Karyawan karyawan4 = new Karyawan(2, 3301, "Nednod", 8000000);
+            karyawan4.gajinaik();
+
+            Console.ReadKey(true)
+        }
+    }
+}
